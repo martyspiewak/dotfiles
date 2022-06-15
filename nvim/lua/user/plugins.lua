355215cf15ef8817461b15884c5d377aaf835aea
Marty Spiewak <spiewakmarty@gmail.com>
2022-06-15 09:36:58 -0400

    Update bufferline plugin

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -4,7 +4,7 @@ M.plugins = function(use)
   use 'fatih/vim-go'
   use 'preservim/nerdtree'
   use 'tomtom/tcomment_vim'
-  use { 'akinsho/bufferline.nvim', tag = "v1.*", requires = 'kyazdani42/nvim-web-devicons' }
+  use { 'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons' }
 end
 
 return M
